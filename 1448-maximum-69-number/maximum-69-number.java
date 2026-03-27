7013d0ee7a4315dcbb3390c4903750814beec0f8
parthipan-max <parthitechne@gmail.com>
2026-03-27 11:42:40 +0530

    Time: 1 ms (80.35%) | Memory: 42.5 MB (20.67%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String str=Integer.toString(num);
+        char[]arr=str.toCharArray();
+        for(int i=0;i<str.length();i++){
+            if(arr[i]=='6'){
+                arr[i]='9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(arr));
+    }
+}
