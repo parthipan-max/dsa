12fac9ba4a82957b66321397feb8df437af9c26e
parthipan-max <parthitechne@gmail.com>
2026-05-04 17:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (16.04%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int rows=matrix.length;
+        for(int i=0;i<rows;i++){
+            for(int j=i+1;j<rows;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<rows/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][rows-1-j];
+                matrix[i][rows-1-j]=temp;
+            }
+        }
+    }
+}
